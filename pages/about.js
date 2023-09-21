d937aa3a0415776881c766ce2e9802d63acebd7d
Maya <mayajwong8@gmail.com>
2023-09-20 20:13:25 -0400

    inital commit

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p> blah blah blah blah blah</p>
+        </div>
+    );
+}
+
+export default About;
---
+++ pages/contact.js
@@ -0,0 +1,10 @@
+const Contact = () => {
+    return (
+        <div>
+            <h1>Contact</h1>
+            <p> email:  mayajwong8@gmail.com</p>
+        </div>
+    );
+}
+
+export default Contact;
